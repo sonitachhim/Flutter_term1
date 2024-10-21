25c38dcf934083e6cf442ebd4ee930d71f296105
Chhim Sonita <chhimsonita@key-3.local>
2024-10-21 10:27:48 +0700

    commit

---
+++ week2_dart_oop/challange/s2-dart-learning-4.dart
@@ -0,0 +1,14 @@
+class Distance{
+  final int _d1;
+  final int _d2;
+  const Distance(this._d1, this._d2);
+  set _d1(int d1) => this._d1 = d1;
+  int get d1 => this._d1;
+  set _d2(int d2) => this._d2 = d2;
+  int get d2 => this._d2;
+  @override
+  
+}
+void main() {
+  
+}
